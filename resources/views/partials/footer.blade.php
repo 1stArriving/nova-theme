4418353bf991a347cb3ecfae41ed7edc22d3b3cf
Adam Privette <aprivette@gmail.com>
2020-06-13 14:34:46 -0400

    Hard set copyright year to 2020

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,5 @@
 <p class="mt-8 text-center text-xs text-80">
     <a href="https://firstarriving.com" class="text-primary dim no-underline">First Arriving</a>
     <span class="px-1">&middot;</span>
-    &copy; {{ date('Y') }} First Arriving LLC
+    &copy; 2020 First Arriving LLC
 </p>
